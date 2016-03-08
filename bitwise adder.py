ae211b04027c6c8095601ac85dbbbcccf15a3d23
apraetor <matthew.a.shapiro@gmail.com>
2016-03-08 11:57:13 -0500

    Adds two binary numbers bitwise
    Just for fun.

---
+++ bitwise adder.py
@@ -0,0 +1,23 @@
+
+# user input
+strA = int(input("First: "), base=2)
+strB = int(input("Second: "), base=2)
+
+
+# AND of two values, shifted left 1 bit is the "carry"
+def carry(strA, strB):
+    return (strA & strB) << 1
+
+
+# XOR of two values
+def adder(strA, strB):
+    return strA ^ strB
+
+while (strB > 0):
+    strA1 = adder(strA, strB)
+    strB1 = carry(strA, strB)
+    strA = strA1
+    strB = strB1
+
+
+print("Sum: {0:b}".format(strA))
